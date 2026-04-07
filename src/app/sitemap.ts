aea9a0d40e17727e7885168bbf344bce5fab3c8d
iam-rmdhn <ilhamztr016@gmail.com>
2026-04-07 17:09:58 +0700

    feat: add sitemap.ts to define site structure and SEO metadata

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,6 +17,30 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/paket-kemitraan/kagawa-rice-bowl`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/paket-kemitraan/kagawa-coffee-conner`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/paket-kemitraan/kagawa-coffee-rice-bowl-conner`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/paket-kemitraan/rm-nusantara`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/hubungi-kami`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
